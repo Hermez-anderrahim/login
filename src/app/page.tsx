e605cf4e9a97eab8c95d6f1dc0d384c1cf6e2a30
Hermez Abderahim <her.abderrahim.15@gmail.com>
2024-06-20 18:49:40 +0100

    fixed postion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,106 +23,108 @@ export default function Home() {
   };
 
   return (
-    <div className={`container ${isSignUpMode ? "sign-up-mode" : ""}`}>
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form action="" className="sign-in-form">
-            <h2 className="title">Sign In</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" value="Login" className="btn solid" />
-            <p className="social-text">Or Sign in with social platforms</p>
-            <div className="social-media">
-              <a href="#" className="social-icon">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" className="social-icon">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="#" className="social-icon">
-                <i className="fab fa-google"></i>
-              </a>
-              <a href="#" className="social-icon">
-                <i className="fab fa-linkedin-in"></i>
-              </a>
-            </div>
-          </form>
+    <div className="big-container">
+      <div className={`container ${isSignUpMode ? "sign-up-mode" : ""}`}>
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="" className="sign-in-form">
+              <h2 className="title">Sign In</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Login" className="btn solid" />
+              <p className="social-text">Or Sign in with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
 
-          <form action="" className="sign-up-form">
-            <h2 className="title">Sign Up</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" value="Sign Up" className="btn solid" />
-            <p className="social-text">Or Sign up with social platforms</p>
-            <div className="social-media">
-              <a href="#" className="social-icon">
-                <FontAwesomeIcon icon={faFacebookF} />
-              </a>
-              <a href="#" className="social-icon">
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-              <a href="#" className="social-icon">
-                <FontAwesomeIcon icon={faGoogle} />
-              </a>
-              <a href="#" className="social-icon">
-                <FontAwesomeIcon icon={faLinkedinIn} />
-              </a>
-            </div>
-          </form>
-        </div>
-      </div>
-
-      <div className="panels-container">
-        <div className="panel left-panel">
-          <div className="content">
-            <h3>New here?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-              minus natus est.
-            </p>
-            <button
-              className="btn transparent"
-              id="sign-up-btn"
-              onClick={handleSignUpClick}
-            >
-              Sign Up
-            </button>
+            <form action="" className="sign-up-form">
+              <h2 className="title">Sign Up</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Sign Up" className="btn solid" />
+              <p className="social-text">Or Sign up with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <FontAwesomeIcon icon={faFacebookF} />
+                </a>
+                <a href="#" className="social-icon">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+                <a href="#" className="social-icon">
+                  <FontAwesomeIcon icon={faGoogle} />
+                </a>
+                <a href="#" className="social-icon">
+                  <FontAwesomeIcon icon={faLinkedinIn} />
+                </a>
+              </div>
+            </form>
           </div>
-          <Image src={log} width={45} className="image" alt="" />
         </div>
 
-        <div className="panel right-panel">
-          <div className="content">
-            <h3>One of us?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-              minus natus est.
-            </p>
-            <button
-              className="btn transparent"
-              id="sign-in-btn"
-              onClick={handleSignInClick}
-            >
-              Sign In
-            </button>
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>New here?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+                minus natus est.
+              </p>
+              <button
+                className="btn transparent"
+                id="sign-up-btn"
+                onClick={handleSignUpClick}
+              >
+                Sign Up
+              </button>
+            </div>
+            <Image src={log} width={45} className="image" alt="" />
           </div>
-          <Image src={register} width={45} className="image" alt="" />
+
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>One of us?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+                minus natus est.
+              </p>
+              <button
+                className="btn transparent"
+                id="sign-in-btn"
+                onClick={handleSignInClick}
+              >
+                Sign In
+              </button>
+            </div>
+            <Image src={register} width={45} className="image" alt="" />
+          </div>
         </div>
       </div>
     </div>
